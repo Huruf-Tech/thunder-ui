e12e2b47b819fdc8e5abb4ce7564ed62684e8ed3
Saif Ali Khan <saffellikhan@gmail.com>
2026-08-02 00:36:03 +0500

    refactor: i18n in App

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,15 @@ import {
 } from "react-router"
 import { useTranslation } from "react-i18next"
 
-import AppWrapper from "@/core/AppWrapper"
-import { LayoutProvider } from "@/core/layouts/layout-provider"
+import AppWrapper from "@/core/AppWrapper.tsx"
+import { LayoutProvider } from "@/core/layouts/layout-provider.tsx"
 
 /** Create a router with the core routes as the child routes of the root path */
-import { coreRoutes, type TRouteObject } from "@/core/router"
-import { Protected } from "@/core/protected"
-import { SelectTenant } from "@/core/pages/tenant/select-tenant"
-import { NotFound } from "@/core/layouts/shared/not-found"
-import { Onboarding } from "@/components/onboarding"
+import { coreRoutes, type TRouteObject } from "@/core/router.tsx"
+import { Protected } from "@/core/protected.tsx"
+import { SelectTenant } from "@/core/pages/tenant/select-tenant.tsx"
+import { NotFound } from "@/core/layouts/shared/not-found.tsx"
+import { Onboarding } from "@/components/onboarding.tsx"
 
 const router = createBrowserRouter(
   [
